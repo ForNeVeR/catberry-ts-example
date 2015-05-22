dec33b3ab3c87fbd0a4cb2e921298cb77987e3ac
Friedrich von Never <friedrich@fornever.me>
2015-05-22 23:10:00 +0600

    Move stuff around, add gulp support.

---
+++ gulpfile.ts
@@ -0,0 +1,12 @@
+/// <reference path="typings/tsd.d.ts"/>
+
+import gulp = require('gulp');
+import ts = require('gulp-typescript');
+
+gulp.task('typescript', () => {
+  return gulp.src('src/**/*.ts').pipe(ts({
+    module: 'commonjs'
+  })).pipe(gulp.dest('build/'));
+});
+
+gulp.task('default', ['typescript']);
